ed678bddeff24950c7ab8192394c36910f801f88
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-11-05 13:21:40 -0700

    added a terrain, a health meter and a exp meter :neckbeard:

---
+++ Capstone/Assets/Scripts/GameControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control; 
+
+    public float health;
+    public float experience;
+
+	void Awake () {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if(control != this)
+        {
+            Destroy(gameObject);
+        }
+        
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Health: " + health);
+        GUI.Label(new Rect(10, 40, 150, 30), "Experience: " + experience);
+    }
+
+}
