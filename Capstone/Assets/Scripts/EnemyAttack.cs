9091cceeae2e2da0d9bab23e952eb8dacd51bff6
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-11-07 21:03:29 -0700

    added a hammer and hammer animation

--- Capstone/Assets/Scripts/EnemyAttack.cs
+++ Capstone/Assets/Scripts/EnemyAttack.cs
@@ -11,7 +11,7 @@ public class EnemyAttack : MonoBehaviour
 
     GameObject player;
     PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
+    EnemyHealth enemyHealth;
     bool playerInRange;
     float timer;
 
@@ -20,7 +20,7 @@ public class EnemyAttack : MonoBehaviour
 	void Awake () {
         player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = player.GetComponent<PlayerHealth>();
-        //enemyHealth = GetComponent<EnemyHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -42,11 +42,14 @@ public class EnemyAttack : MonoBehaviour
     void Update () {
         timer += Time.deltaTime;
 
-        if(timer >= timeBetweenAttacks && playerInRange /*&& enemyHealth.currentHealth > 0*/)
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
         {
             Attack();
         }
+        if(playerHealth.currentHealth <=0)
+        {
 
+        }
 	}
 
     void Attack() 
---
+++ Capstone/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+	// Use this for initialization
+	void Awake () {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+	}
+
+    public void TakeDamage (int amount)
+    {
+        if (isDead)
+
+            return;
+
+        currentHealth -= amount;
+
+        if(currentHealth <=0)
+        {
+            Death();
+        }
+    }
+
+    void Death ()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+    }
+
+    public void StartSinking () 
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        isSinking = true;
+
+        //ScoreManager.score += scoreValue;
+
+        Destroy(gameObject, 2f);
+    }
+}
