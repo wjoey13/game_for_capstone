16201c76e18185ae89261225ccd80dc99d9529a8
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-11-16 10:07:37 -0700

    added more images

--- Capstone/Assets/EnemyController.cs
+++ Capstone/Assets/EnemyController.cs
@@ -6,15 +6,21 @@ public class EnemyController : MonoBehaviour {
 
     Transform player;
     PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
+    EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
-	// Use this for initialization
-	void Awake () {
+    public enum State
+    {
+        Walk,
+        Attack
+    }
+
+    // Use this for initialization
+    void Awake () {
 
         player = GameObject.FindGameObjectWithTag ("Player").transform;
-        //playerHealth = player.GetComponent<PlayerHealth>();
-        //enemyHealth = GetComponent<EnemyHealth>();
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
 		
 	}
@@ -22,13 +28,13 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
             nav.SetDestination(player.position);
-        //}
-        //else 
-        //{
-        //    nav.enabled = false;
-        //}
+        }
+        else 
+        {
+            nav.enabled = false;
+        }
 	}
 }
